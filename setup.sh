2f07df20038abe66553c5bc9582567d7e42d6eb8
rekk2 <32183761+rekk2@users.noreply.github.com>
2024-07-20 10:19:33 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,10 @@
 
 # Update and install necessary packages
 sudo yum update -y
-sudo yum install -y nodejs npm mongodb-org
+sudo yum install -y nodejs npm
+
+# Install MongoDB
+sudo yum install -y mongodb-org
 
 # Start MongoDB
 sudo systemctl start mongod
@@ -14,5 +17,18 @@ cd "$(dirname "$0")"
 # Install project dependencies
 npm install
 
-# Start the application
-npm start
+# Install dotenv package
+npm install dotenv
+
+# Install PM2 globally
+sudo npm install -g pm2
+
+# Start the application using PM2
+pm2 start server.js --name event-registration
+
+# Save PM2 process list and corresponding environment
+pm2 save
+
+# Ensure PM2 restarts on reboot
+pm2 startup systemd -u ec2-user --hp /home/ec2-user
+sudo env PATH=$PATH:/home/ec2-user/.nvm/versions/node/v$(node -v)/bin /home/ec2-user/.nvm/versions/node/v$(node -v)/lib/node_modules/pm2/bin/pm2 startup systemd -u ec2-user --hp /home/ec2-user
